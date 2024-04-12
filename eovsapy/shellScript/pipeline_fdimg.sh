fab7445d59a8bbc1ebf431b314a8ababa667545e
Sijie Yu <sjyu1988@gmail.com>
2024-04-12 13:48:07 -0400

    Major upgrade EOVSA synoptic imaging pipeline
    - Introduce solar differential rotation correction.
    - Refactor output FITS filename format to 'eovsa.synoptic_daily.yymmddT200000_UTC.s??-??.tb.fits'.

--- eovsapy/shellScript/pipeline_fdimg.sh
+++ eovsapy/shellScript/pipeline_fdimg.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-/bin/bash /common/python/eovsapy/shellScript/pipeline.sh -c 1 > /tmp/pipeline.log 2>&1
+/bin/bash /common/python/eovsapy/shellScript/pipeline.sh -c True > /tmp/pipeline.log 2>&1
 /bin/bash /common/python/eovsapy/shellScript/pipeline_plt.sh > /tmp/pipeline_plt.log 2>&1
-/bin/bash /common/python/eovsapy/shellScript/pipeline_compress.sh -n 1 -O 1 > /tmp/pipeline_compress.log 2>&1
+/bin/bash /common/python/eovsapy/shellScript/pipeline_compress.sh -n True -O True > /tmp/pipeline_compress.log 2>&1
 
