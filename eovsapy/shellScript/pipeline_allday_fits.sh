64288d8896933afac5f39b65dbb63964c9f3aa6c
Sijie Yu <sjyu1988@gmail.com>
2024-10-15 16:44:36 -0400

    update eovsapy path

--- eovsapy/shellScript/pipeline_allday_fits.sh
+++ eovsapy/shellScript/pipeline_allday_fits.sh
@@ -3,4 +3,4 @@
 #source /home/user/.cshrc
 # Run the allday fits pipeline
 source /home/user/.setenv_pyenv38
-/home/user/.pyenv/shims/python /common/python/eovsapy/pipeline_allday_fits.py $argv
+/home/user/.pyenv/shims/python /common/python/eovsapy-src/eovsapy/pipeline_allday_fits.py $argv
